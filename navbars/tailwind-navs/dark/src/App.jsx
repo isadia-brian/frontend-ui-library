1b3353a610513d88a2259e3c2f885001d1f2da52
isadia-brian <developerisadia@gmail.com>
2024-04-28 05:57:11 +0300

    third commit

--- navbars/tailwind-navs/dark/src/App.jsx
+++ navbars/tailwind-navs/dark/src/App.jsx
@@ -2,8 +2,9 @@ import Dark from "./components/Dark";
 
 function App() {
   return (
-    <div className='px-4 py-4'>
+    <div className='px-2 py-2 md:px-4 md:py-4'>
       <Dark />
+      <h1>Hello</h1>
     </div>
   );
 }
